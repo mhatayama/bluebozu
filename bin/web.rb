47815aed499520e2fdeae4dc98d6e7ea72c65d3b
mhatayam <masato.hatayama@gmail.com>
2018-06-13 05:35:47 +0900

    make index page and modify views

--- bin/web.rb
+++ bin/web.rb
@@ -6,20 +6,19 @@ require './lib/bouzu'
 set :static, true
 set :public_folder, "static"
 set :views, "views"
+set :layout => :layout
 
 get '/' do
-  "Index."
+  @title = 'MASATO HATAYAMA'
+  @posts = Post.reverse_order(:date)
+  erb :page_index
 end
 
-get '/category/:category' do
-  "category: " + params['category']
-end
+# get '/category/:category' do
+#   "category: " + params['category']
+# end
 
 get '/:post_id' do
-
-  render = Redcarpet::Render::HTML
-  @markdown = Redcarpet::Markdown.new(render, autolink: true, tables: true)
-
   @post = Post[params['post_id']]
-  erb :index
+  erb :page_post
 end
--- config/config.rb
+++ config/config.rb
@@ -1,7 +1,9 @@
-
 require 'sequel'
+require 'redcarpet'
 
 DB = Sequel.connect('sqlite://db/posts.db')
+REDCARPET = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+  autolink: true, tables: true, fenced_code_blocks: true)
 
 $cfg = {}
 $cfg[:date_format] = "%b-%d-%Y"
