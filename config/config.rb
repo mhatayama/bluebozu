4b1d10e69f8d90cf0b6769e144f62c53e9418d77
mhatayam <masato.hatayama@gmail.com>
2018-06-20 07:48:08 +0900

    change posts data path

--- config/config.rb
+++ config/config.rb
@@ -4,6 +4,5 @@ $cfg[:title] = 'masatohatayama.com'
 $cfg[:date_format] = '%b-%d-%Y'
 $cfg[:posts_per_page] = 5
 
-# $cfg[:sequel_conn] = 'sqlite://db/posts.db'
-$cfg[:posts_path] = '../bluebozu_posts'
+$cfg[:posts_path] = '../bluebozu_data'
 $cfg[:redcarpet_opts] = {autolink: true, tables: true, fenced_code_blocks: true}
